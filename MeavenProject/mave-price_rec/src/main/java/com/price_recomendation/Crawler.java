25f50b3dddf69162e41bcb30e01cef11d9e55328
Andreas Rosenback <rosenba@kth.se>
2018-05-09 10:39:51 +0200

    Restructured crawler
    Easier to read and maintain

--- MeavenProject/mave-price_rec/src/main/java/com/price_recomendation/Crawler.java
+++ MeavenProject/mave-price_rec/src/main/java/com/price_recomendation/Crawler.java
@@ -18,14 +18,109 @@ import org.jsoup.select.Elements;
  */
 public class Crawler {
 
-	public Crawler() {
-	}
-
 	private HashMap<Integer, String> catCode2Str = new HashMap<Integer, String>();
 	private HashMap<String, Integer> catStr2Code = new HashMap<String, Integer>();
 	private ArrayList<String> regions = new ArrayList<String>();
 
+	public Crawler() throws IOException {
+		readCategories();
+		readRegions();
+	}
+	
+	public void crawlPage(String query, String category) throws IOException {
+		
+		query = query.replaceAll("\\s+", "+");
+
+		String url;
+		Document doc;
+		Elements imports;
+		Element numb_hits;
+		int numResults;
+		List<Integer> priceList;
+		List<String> titleList;
+		//HashMap<String, Integer> categoriesMap; //Not necessary??
+		for (String listRegion : regions) {
+			url = "https://www.blocket.se/" + listRegion + "?q=" + query + "&cg=" + catStr2Code.get(category);
+			doc = getDoc(url);
+			// Elements links = doc.select("a[href]");
+			imports = doc.select("link[href]");
+			numb_hits = doc.select("span.num_hits").first();
+			numResults = Integer.parseInt(numb_hits.text());
+			
+			//Prints
+			print("\nImports: (%d)", imports.size());
+			for (Element link : imports) {
+				print(" * %s <%s> (%s)", link.tagName(), link.attr("abs:href"), link.attr("rel"));
+			}
+
+			if(numResults == 0) {
+				System.out.println(" No results");
+			}else {
+				System.out.println(numResults + " results for searched query");
+				priceList = priceList(doc);
+				System.out.println(" Number of ads with prices " + priceList.size());
+				if(priceList.size() == 0) {
+					System.out.println(" The result don't have any price labeled");
+				}else {
+					for(int i = 0; i < priceList.size(); i++) {
+						
+					}
+					//categoriesMap = categoriesMap(doc);
+				}
+			}
+		}
+	}
+	
+	private Document getDoc(String url) throws IOException {
+		print("Fetching %s...", url);
+		Document doc = Jsoup.connect(url).get();
+		return doc;
+	}
+
+	private List<Integer> priceList(Document doc) {
+		Elements prices = doc.select("p[itemprop=price]");
+		List<Integer> priceList = new ArrayList<Integer>();
+		int price;
+		int numAdsPrices = 0;
+		for (Element priceElem : prices) {
+			if (!priceElem.text().equals("")) {
+				price = Integer.parseInt(priceElem.text().split("kr")[0].replaceAll("[^0-9.]", ""));
+				System.out.println(price + " from: " + priceElem.text());
+				priceList.add(numAdsPrices, price);
+				numAdsPrices += 1;
+			}
+		}
+		return priceList;
+	}
+	
+	private List<String> titleList(Document doc){
+		Elements titles = doc.select("h1[itemprop=name]");
+		List<String> titleList = new ArrayList<String>();
+		String title;
+		int numAdsTitles = 0;
+		for(Element titleElem : titles) {
+			if(!titleElem.text().equals("")) {
+				title = titleElem.text();
+				System.out.println("Title is: " + title);
+				titleList.add(numAdsTitles, title);
+				numAdsTitles += 1;
+			}
+		}
+		return titleList;
+	}
+	
+	private HashMap<String, Integer> categoriesMap(Document doc) {
+		Elements categories = doc.select("option.mls[^value]");
+		HashMap<String, Integer> categoriesMap = new HashMap<String, Integer>();
+		for (Element cat : categories) {
+			System.out.println(Integer.parseInt(cat.attr("value")) + " " + cat.text());
+			categoriesMap.put(cat.text(), Integer.parseInt(cat.attr("value")));
+		}
+		return categoriesMap;
+	}
+	
 	public void readCategories() throws IOException {
+		System.out.println("Printing categories");
 		String fileName = "src\\main\\resources\\Categories.txt";
 		BufferedReader buf = new BufferedReader(new InputStreamReader(new FileInputStream(fileName), "UTF8"));
 
@@ -43,8 +138,7 @@ public class Crawler {
 	}
 
 	public void readRegions() throws IOException {
-
-		ArrayList<String> regions = new ArrayList<String>();
+		System.out.println("Printing regions");
 		String fileName = "src\\main\\resources\\Regions.txt";
 		BufferedReader buf = new BufferedReader(new InputStreamReader(new FileInputStream(fileName), "UTF8"));
 
@@ -55,64 +149,6 @@ public class Crawler {
 			line = buf.readLine();
 		}
 		buf.close();
-
-	}
-
-	public void crawlPage(String query, String category) throws IOException {
-
-		readCategories();
-		readRegions();
-
-		query = query.replaceAll("\\s+", "+");
-
-		for (String listRegion : regions) {
-			String url = "https://www.blocket.se/" + listRegion + "?q=" + query + "&cg=" + catStr2Code.get(category);
-			print("Fetching %s...", url);
-			Document doc = Jsoup.connect(url).get();
-			// Elements links = doc.select("a[href]");
-			Elements imports = doc.select("link[href]");
-			Element numb_hits = doc.select("span.num_hits").first();
-
-			int numResults = Integer.parseInt(numb_hits.text());
-
-			print("\nImports: (%d)", imports.size());
-			for (Element link : imports) {
-				print(" * %s <%s> (%s)", link.tagName(), link.attr("abs:href"), link.attr("rel"));
-			}
-
-			if (numResults == 0) {
-				System.out.println(" No results");
-			} else {
-				System.out.println(numResults + " results for searched query");
-				Elements prices = doc.select("p[itemprop=price]");
-
-				List<Integer> priceList = new ArrayList<Integer>();
-				int price;
-				int numAdsPrices = 0;
-				for (Element priceElem : prices) {
-					if (!priceElem.text().equals("")) {
-						price = Integer.parseInt(priceElem.text().split("kr")[0].replaceAll("[^0-9.]", ""));
-						System.out.print(price);
-						System.out.println(" from: " + priceElem.text());
-						priceList.add(numAdsPrices, price);
-						numAdsPrices += 1;
-					}
-				}
-				System.out.println(" Number of ads with prices " + numAdsPrices);
-				if (numAdsPrices == 0) {
-					System.out.println(" The result don't have any price labeled");
-				} else {
-					Elements categories = doc.select("option.mls[^value]");
-					HashMap<String, Integer> categoriesMap = new HashMap<String, Integer>();
-					for (Element cat : categories) {
-						System.out.println(Integer.parseInt(cat.attr("value")) + " " + cat.text());
-
-						categoriesMap.put(cat.text(), Integer.parseInt(cat.attr("value")));
-					}
-
-				}
-			}
-		}
 	}
 
 	private static void print(String msg, Object... args) {
